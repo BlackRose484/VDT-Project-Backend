b878528bfe7c7340579070fc5bb41d87cbb847d5
BlackRose484 <hungnbc2@gmail.com>
2025-06-06 19:28:31 +0700

    update: use streamfier for production

--- src/routes/blogs.ts
+++ src/routes/blogs.ts
@@ -3,39 +3,51 @@ const router = express.Router();
 import BlogController from "../controllers/blogs";
 
 import multer from "multer";
-import path from "path";
+import streamifier from "streamifier";
 import cloudinary from "../utils/cloudinary";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "../public/blog_cover"));
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage: storage, limits: { fileSize: 1000000 } });
 
 const uploadWithCloudinary = async (req: any, res: any, next: any) => {
-  try {
-    const result = await cloudinary.uploader.upload(req.file.path, {
-      folder: "blog_cover",
-      public_id: Date.now().toString(),
-      resource_type: "image",
-    });
+  if (!req.file) {
+    return res
+      .status(400)
+      .json({ success: false, message: "No file uploaded" });
+  }
 
+  try {
+    const streamUpload = () =>
+      new Promise((resolve, reject) => {
+        const stream = cloudinary.uploader.upload_stream(
+          {
+            folder: "blog_cover",
+            public_id: Date.now().toString(),
+            resource_type: "image",
+          },
+          (error, result) => {
+            if (result) {
+              resolve(result);
+            } else {
+              reject(error);
+            }
+          }
+        );
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+      });
+
+    const result = await streamUpload();
     req.cloudinary = result;
     next();
   } catch (err) {
-    console.log(err);
+    console.error(err);
     return res.status(500).json({
       success: false,
       message: "Image upload error",
     });
   }
 };
-
 router.get("/get-all", BlogController.getAllBlogs);
 router.get("/get-latest", BlogController.getLatestBlogs);
 router.get("/get/:id", BlogController.getBlogById);
